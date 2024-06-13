93f71fab6c1840631d5052c80ca5292f96f8d34c
Kayonakiyo <kayonakiyo@gmail.com>
2024-06-12 21:43:57 -0500

    working out text animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
 import styles from './App.module.css';
+import hg_logo from './assets/HG2HQ.png'
 
 const App: Component = () => {
+
+  const handleClick = () => {
+    displayTextWithAnimation("this is a test of scrolling text");
+  };
+  
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
+        <img src={hg_logo} class={styles.logo} alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <code>stuff looks cool like this</code>
         </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+
+
+        <p class={styles.animated_text} id="InitializerText"></p>
+      
+          <button onClick={handleClick}>Click me</button> 
+        
+        
       </header>
     </div>
   );
 };
 
 export default App;
+
+function displayTextWithAnimation(text:string) {
+  const textElement = document.createElement('div');
+  textElement.classList.add(styles.animated_text);
+  textElement.textContent = text;
+  const test1 = document.getElementById("InitializerText");
+  if (test1) {
+    test1.innerHTML = '';  // Clear previous text
+    test1.appendChild(textElement);
+  }
+}
