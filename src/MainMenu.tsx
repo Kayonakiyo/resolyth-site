85940a7ba661840d5e11e5cc462dbce3a9d415c3
Kayonakiyo <kayonakiyo@gmail.com>
2024-07-03 07:48:51 -0500

    new main menu module

--- src/MainMenu.tsx
+++ src/MainMenu.tsx
@@ -1,10 +1,35 @@
-import { Component } from 'solid-js';
+import { Component, onMount, createSignal, Show } from 'solid-js';
+import styles from './MainMenu.module.css';
+import { style } from 'solid-js/web';
 
 const MainMenu: Component = () => {
+    const [check, setCheck] = createSignal(false);
+
+    const mainMenuTextInit = () => {
+        setTimeout(() => {
+          setCheck(true);
+          const welcomeText = document.getElementById("welcome_text");
+          if(welcomeText){
+            welcomeText.remove(); // clear this element from dom
+          }
+        }, 1500)
+    }
+
+    onMount(mainMenuTextInit)
+
     return (
-        <div>
-            <p>Main Menu!</p>
+      <Show when={true} fallback={<p>peanis</p>}>
+        <div class={styles.header} id="main">
+          <p class={styles.text} id="welcome_text">Welcome!</p>
+          <Show when={check()} fallback={<p class={styles.text}></p>}>
+            <p class={styles.text} id="loaded_text">loaded!</p>
+            <div class={styles.screen}>
+              <ol></ol>
+            </div>
+          </Show>
         </div>
+      </Show>
+    
     );
 };
 
