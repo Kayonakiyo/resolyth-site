1fea4a0533e39da009608be4c1519b58f8b6c655
Kayonakiyo <kayonakiyo@gmail.com>
2024-07-12 22:39:22 -0500

    further swag

--- src/ProjectBox.tsx
+++ src/ProjectBox.tsx
@@ -5,18 +5,24 @@ const ProjectBox: Component<{visibility:String, projectNumber:Number}> = (props)
     const [visible, setVisible] = createSignal(props.visibility);
     const [projectName, setProjectName] = createSignal("Project X");
     const [currentColor, setProjectColor] = createSignal("#ffffff");
+    const [secondaryColor, setSecondaryColor] = createSignal("#ffffff")
     const [projectDescription, setProjectDescription] = createSignal("Something has gone amiss.");
+    const [smallPreview, setSmallPreview] = createSignal("src/assets/HG2HQ.png");
 
     const projectInit = () => {
       let name = "Project X";
       let color = "#ffffff";
+      let color2 = "#ffffff";
       let description = "Something has gone amiss.";
+      let smallPreview = "src/assets/HG2HQ.png";
 
       switch(props.projectNumber){
         case 0 : 
-          name = "Project #0";
-          color = "";
+          name = "Sound Voltex Arcade Controller";
+          color = "#f974b7";
+          color2 = "#2efdf7";
           description = "A game with knobs, lasers, and FX buttons that make the rhythm game feel like a futuristic post-digital live set.";
+          smallPreview = "src/assets/sdvx_banner.png";
           break;
         case 1 :
           name = "Project #1";
@@ -26,6 +32,10 @@ const ProjectBox: Component<{visibility:String, projectNumber:Number}> = (props)
           break;
       }
       setProjectName(name);
+      setProjectColor(color);
+      setSecondaryColor(color2);
+      setProjectDescription(description);
+      setSmallPreview(smallPreview);
     }
 
     onMount(projectInit);
@@ -33,10 +43,10 @@ const ProjectBox: Component<{visibility:String, projectNumber:Number}> = (props)
     return (
       <Show when={visible} fallback={<></>}>
         <div class={styles.element} style={{'border-color':currentColor()}}>
-        <img src="src/assets/sdvx_banner.png" alt="Sound Voltex Exceed Gear"></img>
-        <p class={styles.text}>{projectName()}</p>
+        <img src={smallPreview()} alt="Sound Voltex Exceed Gear"></img>
+        <p class={styles.text} style={{'color':currentColor()}}>{projectName()}</p>
           <div class={styles.element2}>
-            <p class={styles.text2}>{projectDescription()}</p>
+            <p class={styles.text2} style={{'color':secondaryColor()}}>{projectDescription()}</p>
           </div>
         
         </div>
--- src/Projects.tsx
+++ src/Projects.tsx
@@ -6,12 +6,32 @@ import ProjectBox from './ProjectBox'
 const Projects: Component = () => {
     const [check, setCheck] = createSignal(false);
     const [projectNumber, setProjectNumber] = createSignal(0);
+    const [projectBigLogo, setProjectBigLogo] = createSignal("src/assets/HG2HQ.png");
+    const [previewBorderColor, setPreviewBorderColor] = createSignal("#ffffff")
+
+    createEffect(() => {
+      switch(projectNumber()){
+        case 0: // SDVX
+          setProjectBigLogo("src/assets/sdvx_logo.png");
+          setPreviewBorderColor("#f974b7")
+          break;
+        default:
+          setProjectBigLogo("src/assets/HG2HQ.png");
+      }
+    }, [projectNumber]);
     
     return (
       <Show when={true} fallback={<p>bloopy</p>}>
+        
         <div class={styles.header} id="main">
+          <div class={styles.highlight} style={{'border-color':previewBorderColor()}}>
+            <img src={projectBigLogo()} style={{'position':'relative','bottom':'-75%'}}/>
+          </div>
           <p class={styles.text} id="header_text">This is the projects page.</p>
             <div class={styles.screen}>
+              <video autoplay muted loop >
+                <source src="src/assets/wireworks.mp4"/>
+              </video>
             <ProjectBox visibility="visible" projectNumber={projectNumber()}/>
             </div>
         </div>
