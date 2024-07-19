89a33a78e355b829a49148dfe620f97e5a939ad6
Kayonakiyo <kayonakiyo@gmail.com>
2024-07-19 08:04:35 -0500

    projectpreviews finished!

---
+++ src/PrevBox.tsx
@@ -0,0 +1,64 @@
+import { Component, onMount, createSignal, Show, createEffect } from 'solid-js';
+import styles from './ProjectBox.module.css';
+
+const PrevBox: Component<{visibility:String, projectNumber:Number}> = (props) => {
+    const [visible, setVisible] = createSignal(props.visibility);
+    const [currentColor, setProjectColor] = createSignal("#ffffff");
+    const [secondaryColor, setSecondaryColor] = createSignal("#ffffff")
+    const [smallPreview, setSmallPreview] = createSignal("src/assets/HG2HQ.png");
+
+    const projectInit = () => {
+      let name = "Project X";
+      let color = "#ffffff";
+      let color2 = "#ffffff";
+      let description = "Something has gone amiss.";
+      let smallPreview = "src/assets/HG2HQ.png";
+
+      switch(props.projectNumber){
+        case 0 : 
+          name = "Sound Voltex Arcade Controller";
+          color = "#f974b7";
+          color2 = "#2efdf7";
+          description = "A game with knobs, lasers, and FX buttons that make the rhythm game feel like a futuristic post-digital live set.";
+          smallPreview = "src/assets/sdvx_banner.png";
+          break;
+        case 1 :
+          name = "Beatmania IIDX Arcade Controller";
+          color = "#a73baf";
+          color2 = "#5838f3"; 
+          description = "Spin turntables, keep the dance floor hot!"
+          smallPreview = "src/assets/BMIIDX_Banner.png"
+          break;
+        default :
+          name = "Project X";
+          break;
+      }
+      setProjectName(name);
+      setProjectColor(color);
+      setSecondaryColor(color2);
+      setProjectDescription(description);
+      setSmallPreview(smallPreview);
+    }
+
+    createEffect(() => {
+      projectInit();
+    }, [props.projectNumber])
+    //onMount(projectInit);
+
+    return (
+      <Show when={visible} fallback={<></>}>
+        <div class={styles.element} style={{'border-color':currentColor()}}>
+        <img src={smallPreview()} style={{'width':'100%', 'height':'100%', 'fill':'fit'}}alt={projectName()}></img>
+        <p class={styles.text} style={{'color':currentColor()}}>{projectName()}</p>
+          <div class={styles.element2}>
+            <p class={styles.text2} style={{'color':secondaryColor()}}>{projectDescription()}</p>
+          </div>
+        
+        </div>
+        
+      </Show>
+    
+    );
+};
+
+export default PrevBox;
--- src/Projects.tsx
+++ src/Projects.tsx
@@ -9,6 +9,7 @@ const Projects: Component = () => {
     const [projectBigLogo, setProjectBigLogo] = createSignal("src/assets/HG2HQ.png");
     const [previewBorderColor, setPreviewBorderColor] = createSignal("#ffffff")
     const [backgroundImage, setBackgroundImage] = createSignal("src/assests/HG2HQ.png")
+    const [projectVideo, setProjectVideo] = createSignal("src/assets/gnomed.mp4")
 
     createEffect(() => {
       switch(projectNumber()){
@@ -16,26 +17,29 @@ const Projects: Component = () => {
           setProjectBigLogo("src/assets/sdvx_logo.png");
           setPreviewBorderColor("#f974b7")
           setBackgroundImage("url('src/assets/sdvxbg3.png')")
+          setProjectVideo("src/assets/SDVX_Song.mp4")
           break;
         case 1: //BMIIDX
           setProjectBigLogo("src/assets/BMIIDX_RES.png");
           setPreviewBorderColor("#a73baf")
           setBackgroundImage("url('src/assets/backbanner_IIDXCOLORS.jpg')")
+          setProjectVideo("src/assets/IIDX_Miami.mp4")
           break;
         default:
           setProjectBigLogo("src/assets/HG2HQ.png");
+          setProjectVideo("src/assets/gnomed.mp4")
       }
     }, [projectNumber]);
 
     createEffect(() => {
       const switchProject = (event: { key: string; }) => {
-        if(event.key === 'ArrowDown' || event.key === 'ArrowLeft'){
+        if(event.key === 'ArrowUp' || event.key === 'ArrowLeft'){
           if(projectNumber() > 0){
             setProjectNumber(projectNumber() - 1)
               
           }
-        } else if (event.key === 'ArrowUp' || event.key === 'ArrowRight'){
-          if(projectNumber() < 2){
+        } else if (event.key === 'ArrowDown' || event.key === 'ArrowRight'){
+          if(projectNumber() < 1){
             setProjectNumber(projectNumber() + 1)
           }
         }
@@ -51,8 +55,7 @@ const Projects: Component = () => {
         <div class={styles.header} style={{'background-image':backgroundImage()}}id="main">
           <div class={styles.highlight} style={{'border-color':previewBorderColor()}}>
             <img src={projectBigLogo()} style={{'position':'relative','bottom':'-75%'}}/>
-            <video autoplay muted loop class={styles.screenvideo}>
-              <source src="src/assets/IIDX_Miami.mp4" type="video/mp4"/>
+            <video autoplay muted loop class={styles.screenvideo} id ="vidz" src={projectVideo()}>
             </video>
           </div>
             <div class={styles.screen}>
